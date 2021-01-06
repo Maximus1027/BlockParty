dc662ddada2047aba3e1ec4557ab8d2ed994107b
Maximus1027 <Maximus10272@gmail.com>
2021-01-05 17:43:03 -0800

    Recommendations.

--- src/main/java/me/maximus1027/BlockParty/Main.java
+++ src/main/java/me/maximus1027/BlockParty/Main.java
@@ -3,6 +3,7 @@ package me.maximus1027.BlockParty;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -16,20 +17,14 @@ public class Main extends JavaPlugin {
 
         getConfig().options().copyDefaults();
         saveDefaultConfig();
-        Path path  = Paths.get("plugins/BlockPartyWAX/Floors");
-        try {
-            Files.createDirectories(path);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        File file = new File(getDataFolder(), "Floors/");
 
-
-
-
+        file.mkdirs();
 
 
         new BPHandler(this);
         new BPStick(this);
+        getServer().getPluginManager().registerEvents(new AntiCommands(), this);
         getServer().getPluginManager().registerEvents(new PlayerLoseEvent(), this);
         getServer().getPluginManager().registerEvents(new BPStick(this), this);
     }
